bcc7a1244c77583a6b90651e4dff704484586f52
changhyeon <changhyeon.h@kakao.com>
2024-04-07 22:28:31 +0900

    [websocket-stomp-sock] chore: 채팅 관련 폴더 구조 변경, 코드 일부 수정

--- websocket-stomp-sock/src/main/java/com/study/websocketstompsock/chat/ChatController.java
+++ websocket-stomp-sock/src/main/java/com/study/websocketstompsock/chat/ChatController.java
@@ -5,13 +5,11 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.study.websocketstompsock.chat.dto.UserMessageRequest;
 
 @RestController
-@RequestMapping("/")
 public class ChatController {
 
     @MessageMapping("/message/rooms/{roomId}")
--- websocket-stomp-sock/src/main/java/com/study/websocketstompsock/global/config/ChatConfig.java
+++ websocket-stomp-sock/src/main/java/com/study/websocketstompsock/global/config/ChatConfig.java
@@ -1,4 +1,4 @@
-package com.study.websocketstompsock.chat;
+package com.study.websocketstompsock.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
