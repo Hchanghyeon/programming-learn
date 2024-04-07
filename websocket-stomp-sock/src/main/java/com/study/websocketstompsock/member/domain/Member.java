59b17b438696ecf2cce9bc857ace49c5c9fec3e4
changhyeon <changhyeon.h@kakao.com>
2024-04-07 22:27:00 +0900

    [websocket-stomp-sock] feat: Member 도메인 생성 및 JpaConfig 추가

---
+++ websocket-stomp-sock/src/main/java/com/study/websocketstompsock/member/domain/Member.java
@@ -0,0 +1,42 @@
+package com.study.websocketstompsock.member.domain;
+
+import com.study.websocketstompsock.global.domain.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Builder
+    public Member(final String email, final String password, final Role role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+}
