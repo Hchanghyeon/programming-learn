9abba72baa08bc3820c3c42a001daff46a52125f
changhyeon <changhyeon.h@kakao.com>
2024-04-22 22:26:49 +0900

    [design-pattern] feat: ProxyPattern Cache 구현

--- design-pattern/src/test/java/com/pattern/designpattern/ProxyPatternTest.java
+++ design-pattern/src/test/java/com/pattern/designpattern/ProxyPatternTest.java
@@ -2,8 +2,10 @@ package com.pattern.designpattern;
 
 import org.junit.jupiter.api.Test;
 
+import com.pattern.designpattern.proxy.CacheProxy;
 import com.pattern.designpattern.proxy.ProxyPatternClient;
 import com.pattern.designpattern.proxy.RealSubject;
+import com.pattern.designpattern.proxy.Subject;
 
 public class ProxyPatternTest {
 
@@ -11,6 +13,16 @@ public class ProxyPatternTest {
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient((realSubject));
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void proxyTest(){
+        RealSubject realSubject = new RealSubject();
+        Subject subject = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(subject);
         client.execute();
         client.execute();
         client.execute();
