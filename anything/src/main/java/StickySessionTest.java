caaf364fa9da6f31745caeaad9408e4ade99badc
changhyeon <changhyeon.h@kakao.com>
2024-05-10 18:55:17 +0900

    [anything] feat: StickySessionTest

---
+++ anything/src/main/java/StickySessionTest.java
@@ -0,0 +1,12 @@
+public class StickySessionTest {
+    public static void main(String[] args) {
+        String text = "123.123.123.123";
+        int index = 0;
+        int size = 3;
+
+        String result = text+":"+index;
+
+        System.out.println(result.hashCode());
+        System.out.println(Math.abs(result.hashCode() % size));
+    }
+}
