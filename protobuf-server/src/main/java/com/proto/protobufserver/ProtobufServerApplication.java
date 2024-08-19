efe7fee599c3312784eb7f1e2213690718b1a4c9
changhyeon <changhyeon.h@kakao.com>
2024-08-19 23:43:58 +0900

    build: protobuf server 기본 세팅

---
+++ protobuf-server/src/main/java/com/proto/protobufserver/ProtobufServerApplication.java
@@ -0,0 +1,13 @@
+package com.proto.protobufserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtobufServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtobufServerApplication.class, args);
+    }
+
+}
---
+++ protobuf-server/src/test/java/com/proto/protobufserver/ProtobufServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proto.protobufserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtobufServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
